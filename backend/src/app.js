2665ca3fcb00641edfc371499cd607aa69414d61
anastasigord <wdsa9444@gmail.com>
2026-05-17 15:00:29 +0300

    Add first train

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,3 +3,13 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+
+app.get("/",(_req,res)=>{
+
+res.json({
+status:"Railway backend running"
+});
+
+});
+
+module.exports=app;
---
+++ backend/src/data/trains.js
@@ -0,0 +1,10 @@
+const trains=[{
+id:"train-091k",
+number:"091К",
+from:"Київ",
+to:"Львів"
+}
+];
+
+module.exports=trains;
+
