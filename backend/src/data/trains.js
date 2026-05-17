efb0497726bad49f3663be5bfdc7140e6582f224
anastasigord <wdsa9444@gmail.com>
2026-05-17 15:43:18 +0300

    Extend train data for lab requirements

--- backend/src/data/trains.js
+++ backend/src/data/trains.js
@@ -1,15 +1,30 @@
-const trains=[{
+const trains=[
+    
+{
 id:"train-091k",
 number:"091К",
 from:"Київ",
-to:"Львів"
+to:"Львів",
+departure: "08:00",
+duration: "5 год"
 },
 
 {
 id:2,
 number:"IC145",
 from:"Дніпро",
-to:"Одеса"
+to:"Одеса",
+departure: "12:30",
+duration: "7 год"
+},
+
+{
+id: 3,
+number: "IC332",
+from: "Харків",
+to: "Київ",
+departure: "17:00",
+duration: "4 год"
 }
 
 ];
